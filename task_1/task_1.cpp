e424e36c9a16f81ce5a44213de149187c7b5a9bd
FilipHusnjak <filip.husnjak@fer.hr>
2021-12-01 13:17:40 +0100

    Implement first task

--- task_1/task_1.cpp
+++ task_1/task_1.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <vector>
+
+int first_part(const std::vector<int>& depths) {
+    int count = 0;
+    for (int i = 1; i < depths.size(); i++) {
+        count += depths[i] > depths[i - 1];
+    }
+    return count;
+}
+
+int second_part(const std::vector<int>& depths) {
+    int count = 0;
+    for (int i = 3; i < depths.size(); i++) {
+        count += depths[i] > depths[i - 3];
+    }
+    return count;
+}
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int depth;
+    std::vector<int> depths;
+    while (std::cin >> depth) {
+        depths.push_back(depth);
+    }
+
+    std::cout << "First part: " << first_part(depths) << std::endl;
+    std::cout << "Second part: " << second_part(depths) << std::endl;
+
     return 0;
 }
