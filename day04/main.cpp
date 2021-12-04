6cf2d343fb3aac9b8a1ac7caeebe3373ecc42824
FilipHusnjak <filip.husnjak@fer.hr>
2021-12-05 00:19:42 +0100

    Board numbers are unique

--- day04/main.cpp
+++ day04/main.cpp
@@ -12,12 +12,12 @@ struct board {
         std::memset(columns, 5, sizeof(columns));
     }
 
-    bool operator<(const board &other) {
+    bool operator<(const board &other) const {
         return place < other.place;
     }
 
     uint32_t place = 0;
-    std::unordered_map<int32_t, std::vector<std::pair<uint8_t, uint8_t>>> m{};
+    std::unordered_map<int32_t, std::pair<uint8_t, uint8_t>> m{};
     uint8_t rows[5]{};
     uint8_t columns[5]{};
 };
@@ -26,7 +26,7 @@ auto get_score(int32_t last, const board &b) {
     int score = 0;
     for (auto &&[k, v]: b.m) {
         if (k == last) continue;
-        score += (int32_t) k * (int32_t) v.size();
+        score += (int32_t) k;
     }
     return score * last;
 }
@@ -35,8 +35,7 @@ auto calculate_min_max(const std::vector<int32_t> &nums, std::vector<board> boar
     for (uint32_t i = 0; i < nums.size(); i++) {
         for (auto &&b: boards) {
             if (b.place > 0 || !b.m.contains(nums[i])) continue;
-            for (auto &&pos: b.m.at(nums[i]))
-                if (!--b.rows[pos.first] || !--b.columns[pos.second]) b.place = i;
+            if (!--b.rows[b.m.at(nums[i]).first] || !--b.columns[b.m.at(nums[i]).second]) b.place = i;
             b.m.erase(nums[i]);
         }
     }
@@ -52,7 +51,7 @@ auto parse_boards() {
             for (uint8_t row = 0; row < 5; ++row) {
                 int32_t num;
                 if (!(std::cin >> num)) return std::vector<board>(boards.begin(), --boards.end());
-                boards.rbegin()->m[num].push_back({row, col});
+                boards.rbegin()->m[num] = {row, col};
             }
         }
     }
