a725e39dc72fae70f0fe7b91520ca83896ceaf29
FilipHusnjak <filip.husnjak@fer.hr>
2021-12-05 11:13:38 +0100

    Optimize day 4

--- day04/main.cpp
+++ day04/main.cpp
@@ -3,6 +3,7 @@
 #include <unordered_map>
 #include <ranges>
 #include <algorithm>
+#include <chrono>
 
 #include "../common.h"
 
@@ -12,58 +13,66 @@ struct board {
         std::memset(columns, 5, sizeof(columns));
     }
 
-    bool operator<(const board &other) const {
-        return place < other.place;
-    }
-
-    uint32_t place = 0;
+    uint32_t score = 0;
     std::unordered_map<int32_t, std::pair<uint8_t, uint8_t>> m{};
     uint8_t rows[5]{};
     uint8_t columns[5]{};
 };
 
-auto get_score(int32_t last, const board &b) {
-    int score = 0;
-    for (auto &&[k, v]: b.m) {
-        if (k == last) continue;
-        score += (int32_t) k;
-    }
-    return score * last;
-}
-
-auto calculate_min_max(const std::vector<int32_t> &nums, std::vector<board> boards) {
-    for (uint32_t i = 0; i < nums.size(); i++) {
-        for (auto &&b: boards) {
-            if (b.place > 0 || !b.m.contains(nums[i])) continue;
-            if (!--b.rows[b.m.at(nums[i]).first] || !--b.columns[b.m.at(nums[i]).second]) b.place = i;
-            b.m.erase(nums[i]);
+auto calculate_min_max(const std::vector<int32_t> &nums, std::list<board> boards) {
+    int32_t init_size = (int32_t) boards.size(), min_place = (int32_t) nums.size(), max_place = -1;
+    auto min_b = boards.begin(), max_b = boards.begin();
+    for (int32_t i = 0; i < nums.size(); i++) {
+        if (boards.empty()) break;
+        for (auto it = boards.begin(); it != boards.end();) {
+            if (!it->m.contains(nums[i])) {
+                it++;
+                continue;
+            }
+            it->score -= nums[i];
+            auto &&[row, col] = it->m.at(nums[i]);
+            if (!--it->rows[row] || !--it->columns[col]) {
+                min_b = boards.size() == init_size ? it : min_b;
+                max_b = boards.size() == 1 ? it : max_b;
+                min_place = std::min(min_place, i);
+                max_place = std::max(max_place, i);
+                it = boards.erase(it);
+            } else {
+                it++;
+            }
         }
     }
-    const auto &&[res1, res2] = std::minmax_element(boards.begin(), boards.end());
-    return std::make_pair(get_score(nums[res1->place], *res1), get_score(nums[res2->place], *res2));
+    return std::make_pair(min_b->score * nums[min_place], max_b->score * nums[max_place]);
 }
 
 auto parse_boards() {
-    std::vector<board> boards;
+    std::list<board> boards;
     while (true) {
         boards.emplace_back();
         for (uint8_t col = 0; col < 5; ++col) {
             for (uint8_t row = 0; row < 5; ++row) {
                 int32_t num;
-                if (!(std::cin >> num)) return std::vector<board>(boards.begin(), --boards.end());
+                if (!(std::cin >> num)) return std::list<board>(boards.begin(), --boards.end());
                 boards.rbegin()->m[num] = {row, col};
+                boards.rbegin()->score += num;
             }
         }
     }
 }
 
 int main() {
+    auto start = std::chrono::high_resolution_clock::now();
+
     std::string line;
     std::cin >> line;
-    std::vector<int32_t> nums = split(line, ',');
-    std::vector<board> boards = parse_boards();
+    auto nums = split(line, ',');
+    auto boards = parse_boards();
 
     const auto &&[f, s] = calculate_min_max(nums, boards);
+
+    auto end = std::chrono::high_resolution_clock::now();
+
+    std::cout << std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() << std::endl;
 
     std::cout << "First part = " << f << std::endl;
     std::cout << "Second part = " << s << std::endl;
