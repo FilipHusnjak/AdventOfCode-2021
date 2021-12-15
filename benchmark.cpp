e80410146428dd0ad85244b263ae86244a168e9c
FilipHusnjak <filip.husnjak@fer.hr>
2021-12-15 02:24:34 +0100

    Implement support for multiple iterations for benchmarking

--- benchmark.cpp
+++ benchmark.cpp
@@ -20,48 +20,62 @@
 #include "day12/day12.h"
 #include "day13/day13.h"
 
+#define NUM_ITER 10
+
 template<std::invocable Func>
 auto time(Func && f) {
     auto t1 = std::chrono::high_resolution_clock::now();
-    auto res = f();
+    f();
     auto t2 = std::chrono::high_resolution_clock::now();
-    return std::make_pair(res, std::chrono::duration<double, std::chrono::milliseconds::period>(t2 - t1).count());
+    return std::chrono::duration<double, std::chrono::milliseconds::period>(t2 - t1).count();
 }
 
 auto total = 0.0;
 
-#define BENCHMARK(day) { \
+#define BENCHMARK(day, num_iter) { \
     auto filename = std::string("input/") + #day + ".txt"; \
     std::ifstream file(filename);                \
     parse_t input{};       \
     std::string line;      \
     while (std::getline(file, line))                     \
         input.push_back(line);                             \
-    auto parsing = time([&](){return day::parse(input);}); \
-    auto first_part = time([&](){return day::first_part(parsing.first);}); \
-    assert(first_part.first == day::expected_p1());\
-    auto second_part = time([&](){return day::second_part(parsing.first);});\
-    assert(second_part.first == day::expected_p2());\
-    std::cout << std::format("{} {:^8.4f} {:^8.4f} {:^8.4f} {:^8.4f}\n", #day, parsing.second, first_part.second, second_part.second, parsing.second + first_part.second + second_part.second); \
-    total += parsing.second + first_part.second + second_part.second;\
+    auto parsing_time = time([&](){return day::parse(input);});    \
+    for (int i = 0; i < (num_iter) - 1; i++)    \
+        parsing_time += time([&](){return day::parse(input);});    \
+    parsing_time /= (num_iter);\
+    auto parsing_res = day::parse(input);\
+    auto first_time = time([&](){return day::first_part(parsing_res);}); \
+    for (int i = 0; i < (num_iter) - 1; i++)                           \
+        first_time += time([&](){return day::first_part(parsing_res);}); \
+    first_time /= (num_iter);  \
+    auto first_res = day::first_part(parsing_res);\
+    assert(first_res == day::expected_p1());\
+    auto second_time = time([&](){return day::second_part(parsing_res);}); \
+    for (int i = 0; i < (num_iter) - 1; i++)                           \
+        second_time += time([&](){return day::second_part(parsing_res);}); \
+    second_time /= (num_iter);     \
+    auto second_res = day::second_part(parsing_res);\
+    assert(second_res == day::expected_p2());\
+    std::cout << std::format("{} {:^8.4f} {:^8.4f} {:^8.4f} {:^8.4f}\n", #day, parsing_time, first_time, second_time, parsing_time + first_time + second_time); \
+    total += parsing_time + first_time + second_time;\
 }
 
 int main() {
     std::cout << std::format("{:<5} {:^8} {:^8} {:^8} {:^8}   ms\n", "Day", "Parsing", "Part 1", "Part 2", "Sum");
 
-    BENCHMARK(day01)
-    BENCHMARK(day02)
-    BENCHMARK(day03)
-    BENCHMARK(day04)
-    BENCHMARK(day05)
-    BENCHMARK(day06)
-    BENCHMARK(day07)
-    BENCHMARK(day08)
-    BENCHMARK(day09)
-    BENCHMARK(day10)
-    BENCHMARK(day11)
-    BENCHMARK(day12)
-    BENCHMARK(day13)
+    BENCHMARK(day01, NUM_ITER)
+    BENCHMARK(day02, NUM_ITER)
+    BENCHMARK(day03, NUM_ITER)
+    BENCHMARK(day04, NUM_ITER)
+    BENCHMARK(day05, NUM_ITER)
+    BENCHMARK(day06, NUM_ITER)
+    BENCHMARK(day07, NUM_ITER)
+    BENCHMARK(day08, NUM_ITER)
+    BENCHMARK(day09, NUM_ITER)
+    BENCHMARK(day10, NUM_ITER)
+    BENCHMARK(day11, NUM_ITER)
+    BENCHMARK(day12, NUM_ITER)
+    BENCHMARK(day13, NUM_ITER)
 
     std::cout << "Total: " << total << "ms\n";
 }
